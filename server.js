95f6e042d30326a8c1a6b7b9f06a3a61bc2d633c
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-09 21:01:42 +0530

    FIRST COMMIT

---
+++ server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import colors   from'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
+
+//configure env
+dotenv.config();
+
+// databse config
+connectDB();
+
+
+// rest object
+const app = express()
+
+// middleware
+app.use(morgan('dev'))
+app.use(express.json())
+
+
+// routes 
+app.use('api/v1/auth', authRoutes);
+// rest api 
+app.get('/', (req,res) => {
+    res.send('<h1>Welcome to ecommerce app</h1>');
+});
+
+// PORT 
+const PORT  = process.env.PORT || 8080;
+
+// run listen
+app.listen(PORT, () => {
+    console.log(`Server runnig on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+})
